60cc6e1b69761664ba416f5f81a7cca5d452cba8
Ayan Ansari <ayanansari2528@gmail.com>
2026-06-19 18:54:43 +0530

    Add files via upload

---
+++ src/App.js
@@ -0,0 +1,24 @@
+import React, {} from 'react';
+
+function App() {
+  
+  const display = document.getElementById("display");
+  function appendToDisplay(input){
+    display.value += input;
+
+  }
+  function clearDisplay(){
+    display.value ="";
+  }
+
+  function calculate (){
+    try{
+      display.value = eval(display.value);
+    }
+    catch(error){
+      display.value = "Error"
+    }
+  }
+}
+
+export default App;
